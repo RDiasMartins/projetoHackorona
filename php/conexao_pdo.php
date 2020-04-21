9738cc957c1c9d7ad7dddfd8808df401ddb4ae1b
RDiasMartins <renan2204@outlook.com>
2020-04-21 11:28:18 -0300

    Projeto Hackorona v5.3

--- php/conexao_pdo.php
+++ php/conexao_pdo.php
@@ -1,13 +1,13 @@
 <?php
-
+    /*
     try {
       $link = new PDO('mysql:host=localhost:3307;dbname=projeto', 'root', 'usbw');
         $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch(PDOException $e) {
         echo 'ERROR: ' . $e->getMessage();
     }
+    */
 
-    /*
     $user = "bsts_projeto";
     $pass = "123proj456!";
     $db = "bsts_projeto";
@@ -15,5 +15,5 @@
     //$link = mysqli_connect("pluto.ignitionserver.net:3306", $user, $pass, $db);
 
     $link = new PDO("mysql:host=pluto.ignitionserver.net:3306;dbname=$db", $user, $pass);
-    */
+
 ?>
