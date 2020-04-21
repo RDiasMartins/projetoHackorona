8229168f3a5a0dbe532c27c405213aafbfa05ff1
RDiasMartins <renan2204@outlook.com>
2020-04-21 11:37:25 -0300

    Projeto Hackorona v5.3

--- php/conexao_pdo.php
+++ php/conexao_pdo.php
@@ -13,7 +13,8 @@
     $db = "bsts_projeto";
 
     //$link = mysqli_connect("pluto.ignitionserver.net:3306", $user, $pass, $db);
-
+    //uranus.ignitionserver.net
+    
     $link = new PDO("mysql:host=pluto.ignitionserver.net:3306;dbname=$db", $user, $pass);
 
 ?>
